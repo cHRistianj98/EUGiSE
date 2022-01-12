c154d812dc5154d99f0d3881f1ab2960b82f45df
Krystian Kania <242014@student.pwr.edu.pl>
2022-01-12 19:55:29 +0100

    Refactor model building

--- main.py
+++ main.py
@@ -15,13 +15,72 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import ConfusionMatrixDisplay
 from sklearn.metrics import classification_report, confusion_matrix
 
+
+def show_metrics(res):
+    plt.figure(figsize=(12, 16))
+
+    plt.subplot(4, 2, 1)
+    plt.plot(res.history['loss'], label='Loss')
+    plt.plot(res.history['val_loss'], label='val_Loss')
+    plt.title('Loss Function Evolution')
+    plt.legend()
+
+    plt.subplot(4, 2, 2)
+    plt.plot(res.history['accuracy'], label='accuracy')
+    plt.plot(res.history['val_accuracy'], label='val_accuracy')
+    plt.title('Accuracy Function Evolution')
+    plt.legend()
+
+    plt.subplot(4, 2, 3)
+    plt.plot(res.history['precision'], label='precision')
+    plt.plot(res.history['val_precision'], label='val_precision')
+    plt.title('Precision Function Evolution')
+    plt.legend()
+
+    plt.subplot(4, 2, 4)
+    plt.plot(res.history['recall'], label='recall')
+    plt.plot(res.history['val_recall'], label='val_recall')
+    plt.title('Recall Function Evolution')
+    plt.legend()
+    plt.show()
+
+
+def build_model(net_model):
+    # Convolutional Layer
+    net_model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+    net_model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+
+    # Pooling layer
+    net_model.add(MaxPool2D(pool_size=(2, 2)))
+
+    # Dropout layers
+    net_model.add(Dropout(0.25))
+
+    net_model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+    net_model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+    net_model.add(MaxPool2D(pool_size=(2, 2)))
+    net_model.add(Dropout(0.25))
+
+    net_model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+    net_model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
+    net_model.add(BatchNormalization())
+    net_model.add(MaxPool2D(pool_size=(2, 2)))
+    net_model.add(Dropout(0.25))
+
+    net_model.add(Flatten())
+    net_model.add(Dense(128, activation='relu'))
+    net_model.add(Dropout(0.25))
+    net_model.add(Dense(10, activation='softmax'))
+    return net_model
+
+
 # Load the data
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
-print(f"X_train shape: {X_train.shape}")
-print(f"y_train shape: {y_train.shape}")
-print(f"X_test shape: {X_test.shape}")
-print(f"y_test shape: {y_test.shape}")
 
 classes_name = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
 
@@ -33,46 +92,16 @@ X_test = X_test / 255.0
 y_cat_train = to_categorical(y_train, 10)
 y_cat_test = to_categorical(y_test, 10)
 
-print(y_cat_train)
-
 # Building model
 model = Sequential()
-
-# Convolutional Layer
-model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-# Pooling layer
-model.add(MaxPool2D(pool_size=(2, 2)))
-# Dropout layers
-model.add(Dropout(0.25))
-
-model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(Conv2D(filters=128, kernel_size=(3, 3), input_shape=(32, 32, 3), activation='relu', padding='same'))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-# model.add(Dropout(0.2))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.25))
-model.add(Dense(10, activation='softmax'))
+model = build_model(model)
 
 METRICS = [
     'accuracy',
     tf.keras.metrics.Precision(name='precision'),
     tf.keras.metrics.Recall(name='recall')
 ]
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=METRICS)
 print(model.summary())
 
@@ -86,36 +115,7 @@ steps_per_epoch = X_train.shape[0] // batch_size
 r = model.fit(train_generator,
               epochs=50,
               steps_per_epoch=steps_per_epoch,
-              validation_data=(X_test, y_cat_test),
-#               callbacks=[early_stop],
-#               batch_size=batch_size,
-             )
+              validation_data=(X_test, y_cat_test))
 
-plt.figure(figsize=(12, 16))
-
-plt.subplot(4, 2, 1)
-plt.plot(r.history['loss'], label='Loss')
-plt.plot(r.history['val_loss'], label='val_Loss')
-plt.title('Loss Function Evolution')
-plt.legend()
-
-plt.subplot(4, 2, 2)
-plt.plot(r.history['accuracy'], label='accuracy')
-plt.plot(r.history['val_accuracy'], label='val_accuracy')
-plt.title('Accuracy Function Evolution')
-plt.legend()
-
-plt.subplot(4, 2, 3)
-plt.plot(r.history['precision'], label='precision')
-plt.plot(r.history['val_precision'], label='val_precision')
-plt.title('Precision Function Evolution')
-plt.legend()
-
-plt.subplot(4, 2, 4)
-plt.plot(r.history['recall'], label='recall')
-plt.plot(r.history['val_recall'], label='val_recall')
-plt.title('Recall Function Evolution')
-plt.legend()
-plt.show()
-
+show_metrics(r)
 model.save('cnn_epochs.h5')
