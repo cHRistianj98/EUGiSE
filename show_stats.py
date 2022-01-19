924576955b15ec3491a7d71e12ab937e9d056d8d
Krystian Kania <242014@student.pwr.edu.pl>
2022-01-19 20:43:45 +0100

    add new png files

---
+++ show_stats.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from tensorflow import keras
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel(f"{labels[int(predicted_label)]} {100*np.max(predictions_array):2.0f}% ({labels[int(true_label)]})",
+               color=color)
+
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array, int(true_label[i])
+    plt.grid(False)
+    plt.xticks(range(10))
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+
+# Load model
+model = keras.models.load_model('models/conv256.h5')
+
+# Load the data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+classes_name = ['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+# Scale the data
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# Transform target variable into one-hotencoding
+y_cat_train = to_categorical(y_train, 10)
+y_cat_test = to_categorical(y_test, 10)
+
+evaluation = model.evaluate(X_test, y_cat_test)
+print(f'Test Accuracy : {evaluation[1] * 100:.2f}%')
+
+y_pred = model.predict(X_test)
+y_pred = np.argmax(y_pred, axis=1)
+
+# Define the labels of the dataset
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+predictions = model.predict(X_test)
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 8
+num_cols = 5
+num_images = num_rows * num_cols
+plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+    plot_image(i, predictions[i], y_test, X_test)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions[i], y_test)
+plt.tight_layout()
+plt.savefig(fname='results/conv256.png', orientation='landscape')
+plt.show()
